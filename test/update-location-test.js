d724de61f71cf4274b4707a6bad0063a87c608c1
Mattias Petter Johansson <mpj@mpj.me>
2011-11-24 09:10:52 +0100

    Totally awesome test refactoring

--- test/update-location-test.js
+++ test/update-location-test.js
@@ -2,66 +2,63 @@ var vows    = require('vows'),
     assert = require('assert');
 
 var visitManager = require('../visit-manager');
-var MockMongo	 = require('./mock-mongo').MockServer;
+var MockMongo  = require('./mock-mongo').MockServer;
 
 var VisitManager   = visitManager.VisitManager;
 
 // Iterates through the topics of a test suite
 // and returns the one matching the provided type.
 function getTopic(suite, type) {
-	var topics = suite.context.topics;
-	for(var i = 0; i<topics.length; i++) {
-		var topic = topics[i];
-		if (topic instanceof type)
-			return topic;
-	}
-	throw Error('Topic if type '+ type + 'not found');
+  var topics = suite.context.topics;
+  for(var i = 0; i<topics.length; i++) {
+    var topic = topics[i];
+    if (topic instanceof type)
+      return topic;
+  }
+  throw Error('Topic if type '+ type + 'not found');
 }
 
 function assertCollectionCalled(collName) {
-	return function() {
-		var mongo = getTopic(this, MockMongo);
-		assert.isTrue(mongo.collection_called(collName));
-	}
+  return function() {
+    assert.isTrue(
+      this.mock_mongo.collection_called(collName));
+  }
 }
 
 function assertInsertCalled(doc, suite) {
-	var mongo = getTopic(suite, MockMongo);
-	assert.isTrue( 
-		mongo.current_collection.insert_called(doc)
-	);
+  return function() {
+    assert.isTrue(
+      this.mock_mongo.current_collection.insert_called(doc));
+  }
 }
 
+
 function managerSave(id, lat, lon) {
-	return function(manager) {
-		manager.save(id, lat, lon, this.callback);
-	}
+  return function(manager) {
+    manager.save(id, lat, lon, this.callback);
+  }
 }
 
 vows.describe('VisitManager').addBatch({
-    'given that we have a mock mongo server': {
-    	topic: new MockMongo(),
-    
-	    'given a VisitManager exists': {
-	        topic: function(mongo) {
-	        	return new VisitManager(mongo)
-	        },
 
-	        'and we call save with some parameters': {
-	        	topic: managerSave('999991', 123.4, 456.7),
+  'save': {
+    topic: function(mongo) {
+      this.mock_mongo = new MockMongo();
+      return new VisitManager(this.mock_mongo)
+    },
 
-	        	'it should have called collection "visits"': 
-	        		assertCollectionCalled('visits')
-	        	,
+    'given that we call save with some parameters': {
+      topic: managerSave('999991', 123.4, 456.7),
 
-	        	'callback returns true': function(result) {
-	        		assert.equal(result, true);
-	        	}
-	        },
+      'collection called': assertCollectionCalled('visits'),
 
-	        'mongo_insert_should_have_been_called': function(topic){
-	        	assertInsertCalled({ id: '999991', lat: 123.4, lon: 456.7}, this);
-	        }
-	    }
-    }
+      'insert called': assertInsertCalled(
+        { id: '999991', lat: 123.4, lon: 456.7} ),
+
+      'result is true': function(result) {
+        assert.isTrue(result);
+      }
+    },
+  }
+
 }).export(module); // Export the Suite
