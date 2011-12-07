0baf7d29a56d432ce7802c8469982fe38c1ec8f6
Mattias Petter Johansson <mpj@mpj.me>
2011-12-07 09:37:16 +0100

    Filtering out old visits from result

--- routes/friends_nearby.js
+++ routes/friends_nearby.js
@@ -17,26 +17,38 @@ exports.friends_nearby = function(req, res) {
     return;
   }
     
-  //var myLocation = [ 59.35900833063486, 18.04779052734375 ];
   var myLocation = [ lat, lon ];
 
   // Do this while withFriendIds is running.
-  mongo.ensureIndex('visits', { loc: '2d', facebook_id: 1 }, function(){});
+  mongo.ensureIndex('visits', { loc: '2d', ts: 1, facebook_id: 1 }, function(){});
   
   fb.withFriendIds(token, function(friend_ids) {
     withVisitsNearLocation(myLocation, friend_ids, function(visits) {
-      console.log("Got visits, generating output from", visits.length, "visits");
 
-      var nearby_friends = [];
+      // Create a hash of facebook ids mapped
+      // to distance.
+      var dist_map = {};
       for (var i=0;i<visits.length;i++) {
         var row = visits[i];
+        var fb_id = row.obj.facebook_id;
         var distance_meters = parseFloat(row.dis) * 111 * 1000;
+        
+        // Only assign the first, we are sorting by
+        // ts in the query.
+        if (!dist_map[fb_id])
+          dist_map[fb_id] = distance_meters;
+      }
+
+      // Construct an array of the hash
+      var nearby_friends = [];
+      for(key in dist_map)
+      {
         nearby_friends.push({
-            distance:     distance_meters
-          , facebook_id:  row.obj.facebook_id
+            distance: dist_map[key]    
+          , facebook_id: key 
         });
       }
-      console.log("Returning nearby friends response ...");
+      
       res.send({ nearby_friends: nearby_friends });
     });
   });
@@ -47,6 +59,10 @@ exports.friends_nearby = function(req, res) {
 function withVisitsNearLocation(location, friend_ids, callback) {
   mongo.withServer(function(db) {
 
+    // TODO: Doing it like this won't work as the dataset grows,
+    // the returning function would return to big of a set.
+    // Map reduce or prune data.
+
     console.log("Executing withVisitsNearLocation with");
     // 111 km = approx 1 lat/long unit
     var kilometer_in_lat_long_units = 1 / 111;
@@ -54,7 +70,8 @@ function withVisitsNearLocation(location, friend_ids, callback) {
     var queryopts = { 
         geoNear : 'visits', 
         near : location, 
-        query : { facebook_id: { $in : friend_ids } } 
+        query : { facebook_id: { $in : friend_ids } },
+        sort : { ts: -1 }
       };
     console.log("queryopts", queryopts);
 
