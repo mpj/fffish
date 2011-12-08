3fe95401569687c5dd3455c7c30d6b4d48410546
Mattias Petter Johansson <mpj@mpj.me>
2011-12-08 23:02:17 +0100

    Playing around

--- routes/visits.js
+++ routes/visits.js
@@ -1,6 +1,7 @@
 var mongo   = require('../lib/mongo_helpers'),
-    fb      = require('../lib/facebook_helpers');
-
+    withDistancesOfFriends = require('../lib/facebook_helpers').withDistancesOfFriends;
+var fb = require('../lib/facebook_helpers');
+var apns = require('apn');
 
 exports.visits_create = function(req, res){
 
@@ -26,6 +27,40 @@ exports.visits_create = function(req, res){
       res.send('OK');
     });
   });
+
+  withDistancesOfFriends(facebook_token, function(err, friends_distance) {
+    for(var i=0;i<friends_distance.length;i++) {
+      var fd = friends_distance[i];
+      if (fd.distance < 1250) {
+        fb.withUser(facebook_token, fd.facebook_id, function(err, friend) {
+          var apns_token = "381576c9 863c1c5f 2ec39bff bb64e529 f1e45cfc 0480d6df a28ed3e4 bb7896a0";
+          options =   { cert: '../certificates/apns-dev-cert.pem' /* Certificate file */
+            , key:  '../certificates/apns-dev-key.pem'  /* Key file */
+            , gateway: 'gateway.push.apple.com' /* gateway address */
+            , port: 2195 /* gateway port */
+            , enhanced: true /* enable enhanced format */
+            , errorCallback: 
+                function(error_code, obj) { 
+                  console.log("Apple returned error: ", error_code, "obj sent:" obj);
+                } /* Callback when error occurs */
+            , cacheLength: 5 /* Notifications to cache for error purposes */
+            };
+            var apnsConnection = new apns.connection(options);
+            var myDevice = new apns.device(apns_token);
+
+            var note = new apns.notification();
+
+            note.badge = 1;
+            note.alert = friend['name'] + " is nearby.";
+            note.device = myDevice;
+
+            apnsConnection.sendNotification(note);
+        });
+        break;
+      } 
+    }
+  })
+
 };
 
 function createVisit(facebook_id, lon, lat, callback) {
